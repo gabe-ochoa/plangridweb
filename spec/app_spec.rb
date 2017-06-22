673ee27ab42f9cdd66b06bf1e2b4bcb791b4e8fb
Gabe Ochoa <gabeochoa@gmail.com>
2017-06-22 19:42:47 -0400

    actually send a test header

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe PlanGridWeb::App, type: :controllers do
   describe "An index endpoint '/' where" do
 
     it "GET with an accept header of 'text/html' returns a hello world paragraph" do
+      header 'Accept', 'text/html'
       get('/')
       expect(last_response.status).to be 200
       expect(last_response.body).to eq('<p>Hello, World</p>')
