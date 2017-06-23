2df064c63483bb27f22a50dd6f31f7a384afb6c0
Gabe Ochoa <gabeochoa@gmail.com>
2017-06-22 20:46:44 -0400

    Failing test for POST + SERVER_MODE true

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -20,5 +20,22 @@ RSpec.describe PlanGridWeb::App, type: :controllers do
       expect(last_response.status).to be 200
       expect(last_response.body).to eq('{"message":"Good morning"}')
     end
+
+    context "POST with a JSON body" do
+      let(:json_body) do
+        json_body = {
+          foo: 'value_foo',
+          bar: 'value_bar'
+          }.to_json
+      end
+
+      it "when ENV['SERVER_MODE'] is 'true' return the value for the key 'foo'" do
+        ENV['SERVER_MODE'] = 'true'
+        post '/', json_body
+
+        expect(last_response.status).to be 202
+        expect(last_response.body).to eq('value_foo')
+      end
+    end
   end
 end
